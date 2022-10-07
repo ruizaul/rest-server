eec845b2cfd3c946ce21aa4d3698fd246ec6b924
Saul Fimbres <sfimbres@necodex.com>
2022-10-07 14:05:06 -0700

    first version

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,41 +1,59 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/usuario');
 
-const usuariosGet = (req = request, res = response) => {
-  const { q, nombre = 'No name', apikey, page = 1, limit } = req.query;
+const usuariosGet = async (req = request, res = response) => {
+  const { limite = 5, desde = 0 } = req.query;
+
+  const [total, usuarios] = await Promise.all([
+    Usuario.find({ estado: true }).skip(Number(desde)).limit(Number(limite)),
+    Usuario.count({ estado: true }),
+  ]);
 
   res.json({
-    msg: 'get API - controlador',
-    q,
-    nombre,
-    apikey,
-    page,
-    limit,
+    total,
+    usuarios,
   });
 };
 
-const usuariosPost = (req, res = response) => {
-  const { nombre, edad } = req.body;
+const usuariosPost = async (req, res = response) => {
+  const { nombre, correo, password, rol } = req.body;
+  const usuario = new Usuario({ nombre, correo, password, rol });
 
-  res.json({
-    msg: 'post API - controlador',
-    nombre,
-    edad,
-  });
+  //Encriptar la contraseña
+  const salt = bcryptjs.genSaltSync();
+  usuario.password = bcryptjs.hashSync(password, salt);
+
+  //Guardar registro
+  await usuario.save();
+
+  res.json(usuario);
 };
 
-const usuariosPut = (req, res = response) => {
+const usuariosPut = async (req, res = response) => {
+  const { id } = req.params;
+  const { _id, password, google, correo, ...resto } = req.body;
+
+  if (password) {
+    //Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    resto.password = bcryptjs.hashSync(password, salt);
+  }
+
+  const usuario = await Usuario.findByIdAndUpdate(id, resto);
+
+  res.json(usuario);
+};
+
+const usuariosDelete = async (req, res = response) => {
   const { id } = req.params;
 
-  res.json({
-    msg: 'put API - controlador',
-    id,
-  });
-};
+  // Borrar fisicamente
+  //const usuario = Usuario.findByIdAndDelete(id);
 
-const usuariosDelete = (req, res = response) => {
-  res.json({
-    msg: 'delete API - controlador',
-  });
+  const usuario = await Usuario.findByIdAndUpdate(id, { estado: false });
+
+  res.json(usuario);
 };
 
 module.exports = {
--- models/server.js
+++ models/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
     this.usuariosPath = '/api/usuarios';
+
+    //Conectar a base de datos
+    this.conectarDB();
 
     //Middlewares
     this.middlewares();
@@ -14,14 +18,18 @@ class Server {
     this.routes();
   }
 
+  async conectarDB() {
+    await dbConnection();
+  }
+
   middlewares() {
     //CORS
     this.app.use(cors());
 
-    // Lectura y parseo del body
+    //Lectura y parseo del body
     this.app.use(express.json());
 
-    // Directorio Publico
+    //Directorio Publico
     this.app.use(express.static('public'));
   }
 
