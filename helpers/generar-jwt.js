7f9b19aa66074935c977624a0e6c85b9d9e8d0e9
Saul Fimbres <sfimbres@necodex.com>
2022-11-03 00:34:57 -0700

    w

--- helpers/generar-jwt.js
+++ helpers/generar-jwt.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const generarJWT = (uid = '') => {
   return new Promise((resolve, reject) => {
-    const payload = { uid };
+    const payload = { uid, nombre, correo };
 
     jwt.sign(
       payload,
